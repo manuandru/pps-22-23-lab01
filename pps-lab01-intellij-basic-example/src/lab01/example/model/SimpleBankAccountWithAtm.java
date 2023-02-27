cfd33a617285a6101ab2eb5a2c117d38cf751658
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-02-27 12:09:38 +0100

    refactor implementation

--- pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -3,45 +3,32 @@ package lab01.example.model;
 public class SimpleBankAccountWithAtm implements BankAccount {
 
     protected static final int ATM_FEE = 1;
-    private final AccountHolder holder;
-    private double balance;
+    private final BankAccount delegate;
 
     public SimpleBankAccountWithAtm(final AccountHolder holder, final double balance) {
-        this.holder = holder;
-        this.balance = balance;
+        this.delegate = new SimpleBankAccount(holder, balance);
     }
 
     @Override
     public AccountHolder getHolder() {
-        return this.holder;
+        return this.delegate.getHolder();
     }
 
     @Override
     public double getBalance() {
-        return this.balance;
+        return this.delegate.getBalance();
     }
 
     @Override
     public void deposit(int userID, double amount) {
-        if (checkUser(userID) && amount > ATM_FEE) {
-            this.balance += amount;
-            this.balance -= ATM_FEE;
+        if (amount > ATM_FEE) {
+            this.delegate.deposit(userID, amount - ATM_FEE);
         }
     }
 
     @Override
     public void withdraw(int userID, double amount) {
-        if (checkUser(userID) && isWithdrawAllowed(amount)) {
-            this.balance -= amount;
-            this.balance -= ATM_FEE;
-        }
-    }
-
-    private boolean isWithdrawAllowed(final double amount){
-        return this.balance >= amount + ATM_FEE;
-    }
-    private boolean checkUser(int userID) {
-        return this.holder.getId() == userID;
+        this.delegate.withdraw(userID, amount + ATM_FEE);
     }
 
 }
