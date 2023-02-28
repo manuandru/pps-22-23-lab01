e99e91e7173f1a505ed74410b713a85cc54087fd
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-02-28 19:34:34 +0100

    initially empty list

---
+++ pps-lab01-tdd/test/lab01/tdd/CircularListTest.java
@@ -0,0 +1,25 @@
+package lab01.tdd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The test suite for testing the CircularList implementation
+ */
+public class CircularListTest {
+
+    private CircularList list;
+
+    @BeforeEach
+    void setUp() {
+        list = new CircularListImpl();
+    }
+
+    @Test
+    void testInitiallyEmpty() {
+        assertTrue(list.isEmpty());
+    }
+
+}
